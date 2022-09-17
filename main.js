b8f79c4f86244effceeb10cf7fe253c1748c19c4
양정연 <wjdduscldrn@gmail.com>
2022-09-17 19:49:17 +0900

    [Fix] del 코드 수정

--- main.js
+++ main.js
@@ -6,7 +6,22 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+function filterFn(toDo) {
+    return toDo.id === 1; //filter는 array의 모든 아이템을 통해 함수를 실행하고 true인 아이템들만 가지고 새로운 array를 만든다
+}
+
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function (toDo) {
+        return toDo.id !== parseInt(li.id); //parseInt 스트링을 숫자로 바꿔줌
+    }); //claenTOdos와 filter가 하는 것은 filterFn이 체크가 된 아이템들의 array를 주는것
+    toDos = cleanToDos; // 얘때문에 const toDos를 let으로 바꿈 
+    saveToDOs();
+}
 
 function saveToDOs() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -16,19 +31,11 @@ function paintTodo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "❌";
-    delBtn.addEventListener('click', () => {
-        li.remove(span);
-    })
-    const finishBtn = document.createElement("button");
-    finishBtn.innerText = "✔️";
-    finishBtn.addEventListener('click', () => {
-        span.style.textDecorationLine = "line-through";
-    })
+    delBtn.addEventListener('click', deleteToDo);
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     span.innerText = text
     li.appendChild(span);
-    li.appendChild(finishBtn);
     li.appendChild(delBtn);
     li.id = newId;
     toDoList.appendChild(li);
